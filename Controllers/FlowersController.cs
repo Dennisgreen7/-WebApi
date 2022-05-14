a69d60501d19fba99f4ebdffdea83948f82d59e1
Dennisgreen7 <denis4167@gmail.com>
2022-05-14 18:19:11 +0300

    first commit

---
+++ Controllers/FlowersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlowersApi.Controllers
+{
+    [ApiController]
+    [Route("Flowers/[action]")]
+    public class FlowersController: ControllerBase
+    {
+        public string Rose()
+        {
+            return "Rose";
+        }
+        public string Orchid()
+        {
+            return "Orchid";
+        }
+        public string Daisy()
+        {
+            return "Daisy";
+        }
+        public string Iris()
+        {
+            return "Iris";
+        }
+    }
+}
